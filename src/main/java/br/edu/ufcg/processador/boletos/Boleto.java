721296e33c25cc73cc3f616ea2bf4b5b9a093679
Helen <helen.cavalcanti@ccc.ufcg.edu.br>
2023-04-12 13:22:53 -0300

    Refatorando

--- src/main/java/br/edu/ufcg/processador/boletos/Boleto.java
+++ src/main/java/br/edu/ufcg/processador/boletos/Boleto.java
@@ -2,12 +2,13 @@ package br.edu.ufcg.processador.boletos;
 
 public class Boleto {
     private int valor;
-
+    
     public Boleto(Integer valor){
         this.valor = valor;
 
     }
 
+    // recupera valor do boleto
     public Object getValor() {
         return valor;
     }
