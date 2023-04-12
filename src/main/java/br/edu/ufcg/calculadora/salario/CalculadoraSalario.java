f439ddede0741fea569321afdfd1644ac8060186
Henrique Lopes <henriquelopees7@gmail.com>
2023-04-11 22:46:39 -0300

    green

--- src/main/java/br/edu/ufcg/calculadora/salario/CalculadoraSalario.java
+++ src/main/java/br/edu/ufcg/calculadora/salario/CalculadoraSalario.java
@@ -13,6 +13,9 @@ import java.math.BigDecimal;
 public class CalculadoraSalario {
 
     public BigDecimal calculaSalario(Funcionario funcionario) {
+        if (funcionario.getSalarioBase().compareTo(new BigDecimal("3000")) >= 0) {
+            return funcionario.getSalarioBase().multiply(BigDecimal.valueOf(0.8));
+        }
         return funcionario.getSalarioBase().multiply(BigDecimal.valueOf(0.9));
     }
 }
