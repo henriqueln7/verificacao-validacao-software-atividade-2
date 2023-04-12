75fa4410567598c460280b92175753cb8ab0dddc
Helen <helen.cavalcanti@ccc.ufcg.edu.br>
2023-04-12 14:15:36 -0300

    melhorando teste

--- src/test/java/br/edu/ufcg/ControllerFaturaTest.java
+++ src/test/java/br/edu/ufcg/ControllerFaturaTest.java
@@ -1,5 +1,6 @@
-package br.edu.ufcg;
 import org.junit.jupiter.api.Test;
+import br.edu.ufcg.processador.boletos.Boleto;
+import br.edu.ufcg.processador.boletos.ControllerFatura;
 import br.edu.ufcg.processador.boletos.Fatura;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,18 +10,18 @@ class ControllerFaturaTest{
     private ControllerFatura controllerfatura;
 
     @Test
-    public void verificaStatusControllerFatura() {
-        controllerfatura = new ControllerFatura();
-        boleto = new Boleto(1, "12-04-2023", 500);
+    public void pagaFaturaControllerFatura() {
+         controllerfatura = new ControllerFatura();
+        Boleto boleto = new Boleto(1, "12-04-2023", 500);
         controllerfatura.adicionaBoleto(boleto);
-        boleto1 = new Boleto(2, "12-04-2023", 500);
+        Boleto boleto1 = new Boleto(2, "12-04-2023", 500);
         controllerfatura.adicionaBoleto(boleto1);
-        boleto2 = new Boleto(3, "12-04-2023", 600);
+        Boleto boleto2 = new Boleto(3, "12-04-2023", 600);
         controllerfatura.adicionaBoleto(boleto2);
-        fatura = new Fatura("12-04-2023", 1500, "Jorge");
+        Fatura fatura = new Fatura("12-04-2023", 1500, "Jorge");
         controllerfatura.pagarFatura(fatura);
+        assertEquals("PAGA", fatura.getStatus());
 
-        
     
     }
 }
