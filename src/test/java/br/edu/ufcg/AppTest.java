d9fd7e06cdd4a236fa7634c27ccea32b9b24cca7
Henrique Lopes <henriquelopees7@gmail.com>
2023-04-11 22:20:23 -0300

    Olá mundo =)

---
+++ src/test/java/br/edu/ufcg/AppTest.java
@@ -0,0 +1,13 @@
+package br.edu.ufcg;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AppTest {
+
+    @Test
+    void testApp() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
