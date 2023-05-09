f06b69fda03a552548488419ea238279f0619fbc
Henrique Lopes <henriquelopees7@gmail.com>
2023-05-09 14:25:29 -0300

    Corrige testes

--- src/test/java/br/edu/ufcg/calculadora/salario/TestsFuncionaisCalculadoraSalario.java
+++ src/test/java/br/edu/ufcg/calculadora/salario/TestsFuncionaisCalculadoraSalario.java
@@ -1,5 +1,6 @@
 package br.edu.ufcg.calculadora.salario;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,29 +10,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class TestsFuncionaisCalculadoraSalario {
 
+    private CalculadoraSalario calculadoraSalario;
+
+    @BeforeEach
+    void setUp() {
+        this.calculadoraSalario = new CalculadoraSalario();
+    }
+
     @ParameterizedTest
     @CsvSource(textBlock = """
             DESENVOLVEDOR,Helen,helen@gmail.com,3000.0,2400.0
-            DESENVOLVEDOR,Helen,helen@gmail.com,2500.0,2225.0
-            DBA,Helen,helen@gmail.com,2000.0,1750.0
-            DBA,Helen,helen@gmail.com,1500.0,1275.0
-            TESTADOR,Helen,helen@gmail.com,2000.0,1750.0
-            TESTADOR,Helen,helen@gmail.com,1500.0,1275.0
-            GERENTE,Helen,helen@gmail.com,5000.0,3500.0
-            GERENTE,Helen,helen@gmail.com,4500.0,3600.0
-            DESENVOLVEDOR,Helen,helen@gmail.com,2999.99,2699.991
-            DESENVOLVEDOR,Helen,helen@gmail.com,3000.01,2400.0075
-            DBA,Helen,helen@gmail.com,1999.99,1499.9925
-            DBA,Helen,helen@gmail.com,2000.01,1700.0075
-            TESTADOR,Helen,helen@gmail.com,1999.99,1499.9925
-            TESTADOR,Helen,helen@gmail.com,2000.01,1700.0075
-            GERENTE,Helen,helen@gmail.com,4999.99,3499.993
-            GERENTE,Helen,helen@gmail.com,5000.01,3500.0075
+           DESENVOLVEDOR,Helen,helen@gmail.com,2500.0,2250.0
+           DBA,Helen,helen@gmail.com,2000.0,1500.0
+           DBA,Helen,helen@gmail.com,1500.0,1275.0
+           TESTADOR,Helen,helen@gmail.com,2000.0,1500.0
+           TESTADOR,Helen,helen@gmail.com,1500.0,1275.0
+           GERENTE,Helen,helen@gmail.com,5000.0,3500.0
+           GERENTE,Helen,helen@gmail.com,4500.0,3600.0
+           DESENVOLVEDOR,Helen,helen@gmail.com,2999.99,2699.991
+           DESENVOLVEDOR,Helen,helen@gmail.com,3000.01,2400.008
+           DBA,Helen,helen@gmail.com,1999.99,1699.9915
+           DBA,Helen,helen@gmail.com,2000.01,1500.0075
+           TESTADOR,Helen,helen@gmail.com,1999.99,1699.9915
+           TESTADOR,Helen,helen@gmail.com,2000.01,1500.0075
+           GERENTE,Helen,helen@gmail.com,4999.99,3999.992
+           GERENTE,Helen,helen@gmail.com,5000.01,3500.007
             """)
     void testCalculadoraSalario(Cargo cargo, String nome, String email, BigDecimal salarioBase, BigDecimal salarioComDesconto) {
-        CalculadoraSalario calculadoraSalario = new CalculadoraSalario();
         Funcionario funcionario = new Funcionario(nome, salarioBase, cargo);
+
         BigDecimal resultado = calculadoraSalario.calculaSalario(funcionario);
+
         assertThat(resultado).isEqualByComparingTo(salarioComDesconto);
     }
 }
