51e34593dd1fa475e115d8b4066cc62ae1bc7464
Henrique Lopes <henriquelopees7@gmail.com>
2023-06-21 08:13:24 -0300

    assertj

--- src/test/java/br/edu/ufcg/functionalTests/ProcessadorBoletosTesteFuncional.java
+++ src/test/java/br/edu/ufcg/functionalTests/ProcessadorBoletosTesteFuncional.java
@@ -1,16 +1,18 @@
-package br.edu.ufcg.functionalTests;
+scpackage br.edu.ufcg.functionalTests;
 
 import br.edu.ufcg.processador.boleto.Boleto;
 import br.edu.ufcg.processador.boleto.Fatura;
 import br.edu.ufcg.processador.boleto.Pagamento;
 import br.edu.ufcg.processador.boleto.ProcessadorBoletos;
 import br.edu.ufcg.processador.boleto.Pagamento.TipoPagamento;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -34,11 +36,24 @@ public class ProcessadorBoletosTesteFuncional {
         assertFalse(fatura.isPaga());
 
         List<Pagamento> pagamentos = fatura.getPagamentos();
-        assertEquals(2, pagamentos.size());
-        assertEquals(TipoPagamento.BOLETO, pagamentos.get(0).getTipo());
-        assertEquals(TipoPagamento.BOLETO, pagamentos.get(1).getTipo());
-        assertEquals(pagamentos.get(0).getValorPago(), new BigDecimal("800.00"));
-        assertEquals(pagamentos.get(1).getValorPago(), new BigDecimal("699.00"));
+
+        assertThat(fatura.getValorTotal()).isEqualTo(new BigDecimal("1500.00"));
+        assertThat(fatura.isPaga()).isFalse();
+//        assertEquals(fatura.getValorTotal(), new BigDecimal("1500.00"));
+//        assertFalse(fatura.isPaga());
+
+        assertThat(pagamentos).hasSize(2).satisfies(pagamento -> {
+            assertThat(pagamento.getTipo()).isEqualTo(TipoPagamento.BOLETO);
+            assertThat(pagamento.getValorPago()).isEqualTo(new BigDecimal("800.00"));
+        }, atIndex(0)).satisfies(pagamento -> {
+            assertThat(pagamento.getTipo()).isEqualTo(TipoPagamento.BOLETO);
+            assertThat(pagamento.getValorPago()).isEqualTo(new BigDecimal("699.00"));
+        }, atIndex(1));
+//        assertEquals(pagamentos.size(), 2);
+//        assertEquals(pagamentos.get(0).getTipo(), TipoPagamento.BOLETO);
+//        assertEquals(pagamentos.get(1).getTipo(), TipoPagamento.BOLETO);
+//        assertEquals(pagamentos.get(0).getValorPago(), new BigDecimal("800.00"));
+//        assertEquals(pagamentos.get(1).getValorPago(), new BigDecimal("699.00"));
     }
 
     @Test
